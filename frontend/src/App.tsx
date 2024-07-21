90e6944fa3848ef8a1da0cf009fb47e307c070a4
KuuLotus <kuulotus.dev@gmail.com>
2024-07-21 17:21:35 +0900

    add tanstack query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import {
   Card,
   CardContent,
@@ -7,17 +6,26 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-function App() {
-  const [totalSpent, setTotalSpent] = useState(0);
+import { useQuery } from "@tanstack/react-query";
 
-  useEffect(() => {
-    async function fetchTotal() {
-      const res = await fetch("/api/expenses/total-spent");
-      const data = await res.json();
-      setTotalSpent(data.total);
-    }
-    fetchTotal();
-  }, []);
+import { api } from "@/lib/api";
+
+async function getTotalSpent() {
+  const res = await api.expenses["total-spent"].$get();
+  if (!res.ok) {
+    throw new Error("server error");
+  }
+  const data = await res.json();
+  return data;
+}
+
+function App() {
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ["total-spent"],
+    queryFn: getTotalSpent,
+  });
+
+  if (error) return "An error has occurred: " + error.message;
 
   return (
     <Card className="w-[350px] m-auto">
@@ -25,7 +33,7 @@ function App() {
         <CardTitle>Total Spent</CardTitle>
         <CardDescription>The total amount you've spent</CardDescription>
       </CardHeader>
-      <CardContent>{totalSpent}</CardContent>
+      <CardContent>{isPending ? "..." : data.total}</CardContent>
     </Card>
   );
 }
